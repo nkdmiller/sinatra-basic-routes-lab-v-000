d328d5a58d822a6d54c85012dc2c1982a3054f97
Nate Miller <natekmiller96@gmail.com>
2018-08-11 19:07:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nate"
+  end
+  get '/hometown' do
+    "My hometown is Carol Stream"
+  end
+  get '/favorite-song' do
+    "My favorite song is All Along the Watchtower"
+  end
 end
